800583663b6cff9fcff25162f36584e9047fec90
Cody Groves <codygroves@Codys-MacBook-Air.local>
2026-02-08 09:44:57 -0500

    Fix iOS wrapper to load local dist

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.b8261d4e5f8d406f98bb2e235eb6b0ad',
-  appName: 'GoodCreation Browser',
+  appId: 'bet.goodcreation.miracleworker',
+  appName: 'Miracle Worker',
   webDir: 'dist',
-  server: {
-    url: 'https://b8261d4e-5f8d-406f-98bb-2e235eb6b0ad.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
   plugins: {
     InAppBrowser: {
       // Plugin configuration
