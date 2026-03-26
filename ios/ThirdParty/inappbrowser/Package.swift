59c69ae4a70c63d6d179ff20a1461b28718bc37b
Cody Groves <YOUR_EMAIL@example.com>
2026-03-25 20:31:08 -0400

    Vendoring Capgo Plugin to ios/ThirdParty for Native Patching

---
+++ ios/ThirdParty/inappbrowser/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapgoInappbrowser",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "CapgoInappbrowser",
+            targets: ["InappbrowserPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+    ],
+    targets: [
+        .target(
+            name: "InappbrowserPlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
+            ],
+            path: "ios/Sources/InAppBrowserPlugin"),
+        .testTarget(
+            name: "InappbrowserPluginTests",
+            dependencies: ["InappbrowserPlugin"],
+            path: "ios/Tests/InappbrowserPluginTests")
+    ]
+)
---
+++ ios/ThirdParty/inappbrowser/ios/Sources/InAppBrowserPlugin/Enums.swift
@@ -0,0 +1,65 @@
+//
+//  Enums.swift
+//  Sample
+//
+//  Created by Meniny on 2018-01-20.
+//  Copyright © 2018年 Meniny. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public enum WKWebSource: Equatable {
+    case remote(URL)
+    case file(URL, access: URL)
+    case string(String, base: URL?)
+
+    public var url: URL? {
+        switch self {
+        case .remote(let urlValue): return urlValue
+        case .file(let urlValue, access: _): return urlValue
+        default: return nil
+        }
+    }
+
+    public var remoteURL: URL? {
+        switch self {
+        case .remote(let urlValue): return urlValue
+        default: return nil
+        }
+    }
+
+    public var absoluteString: String? {
+        switch self {
+        case .remote(let urlValue): return urlValue.absoluteString
+        case .file(let urlValue, access: _): return urlValue.absoluteString
+        default: return nil
+        }
+    }
+}
+
+public enum BarButtonItemType {
+    case back
+    case forward
+    case reload
+    case stop
+    case activity
+    case done
+    case flexibleSpace
+    case custom(icon: UIImage?, title: String?, action: (WKWebViewController) -> Void)
+}
+
+public enum NavigationBarPosition: String, Equatable, Codable {
+    case none
+    case left
+    case right
+}
+
+@objc public enum NavigationType: Int, Equatable, Codable {
+    case linkActivated
+    case formSubmitted
+    case backForward
+    case reload
+    case formResubmitted
+    case other
+}
